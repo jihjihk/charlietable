e48f1266e77735fa637ca85d3a1f1fba3f6d02c7
jihjihk <jihyun@nyu.edu>
2017-11-23 15:23:21 +0400

    integrating with expo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import AppViewContainer from './src/modules/AppViewContainer';
 import React, {Component} from 'react';
+import 'expo';
 import {
   Alert,
   AppRegistry,
