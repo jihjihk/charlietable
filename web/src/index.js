4b4019c9b4c984450bcae6c8cfcf7a30cec89fa3
jihjihk <jihyun@nyu.edu>
2017-11-29 11:48:35 +0400

    home scene reorganized

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.jsx';
+
+//import "babel-polyfill";
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
 registerServiceWorker();
-
