f926048f9a9fe721f534a401cf13a4eedfee45fe
Seongun <ss8355@nyu.edu>
2017-12-01 22:41:21 +0400

    updating newEvent form

--- web/src/scenes/NewEvent/index.jsx
+++ web/src/scenes/NewEvent/index.jsx
@@ -114,12 +114,6 @@ export default class NewEvent extends Component {
 
     return (
       <div>
-        <Segment
-          inverted
-          textAlign='center'
-          style={{ minHeight: 700, padding: '1em 0em' }}
-          vertical
-        >
           <Container>
             <Menu fixed='top' size='large'>
               <Menu.Item as='home' active>Home</Menu.Item>
@@ -136,72 +130,35 @@ export default class NewEvent extends Component {
                     My Page
                   </Menu.Item>
                 }
-              }
+              
 
               <Menu.Menu position='right'>
                 <Menu.Item className='item'>
                   {this.state.user ?
-                      <Button as='logout' onClick={this.logout}>Log Out</Button>
-                      :
-                      <Button as='login' onClick={this.login}>Log In</Button>
-                    }
+                    <Button as='logout' onClick={this.logout}>Log Out</Button>
+                    :
+                    <Button as='login' onClick={this.login}>Log In</Button>
+                  }
                 </Menu.Item>
               </Menu.Menu>
           </Menu>
         </Container>
 
-
-        <Container text>
-          <Header
-            as='h1'
-            content='The Dinner Party You Choose'
-            inverted
-            style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
-          />
-          <Header
-            as='h2'
-            content='Curated dinners with 3 interesting strangers like you'
-            inverted
-            style={{ fontSize: '1.7em', fontWeight: 'normal' }}
-          />
-          <Button primary size='huge'>
-            Find Dinners
-            <Icon name='right arrow' />
-          </Button>
-        </Container>
-      </Segment>
-
         <Segment style={{ padding: '8em 0em' }} vertical>
           <Container>
             <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
               <div className='container'>
-                <section className='add-item'>
+                <section className='createEventInput'>
                   <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+                    <input type="text" name="eventName" placeholder="Name this event" onChange={this.handleChange} value={this.state.eventName} />
                     <input type="text" name="cuisine" placeholder="What cuisine do you like?" onChange={this.handleChange} value={this.state.cuisine} />
-                    <input type="text" name="interest" placeholder="What are you interested in?" onChange={this.handleChange} value={this.state.interest} />
-
-                    <button>Tell us</button>
+                    <input type="text" name="location" placeholder="Where do you want to host this event?" onChange={this.handleChange} value={this.state.location} />                      <input type="text" name="interest" placeholder="What are you interested in?" onChange={this.handleChange} value={this.state.interest} />
+                    <input type="text" name="time" placeholder="When do you wanna host this event?" onChange={this.handleChange} value={this.state.time} />
+                    <button>Create Event</button>
                   </form>
+
                 </section>
-                <section className='display-item'>
-                    <div className="wrapper">
-                      <ul>
-                        {this.state.items.map((item) => {
-                          return (
-                            <li key={item.user}>
-                              <h3>{item.user}</h3>
-                              <p>likes {item.cuisine} food</p>
-                              <p>and is interested in {item.interest}
-                                 {item.user === this.state.user.displayName || item.user === this.state.user.email ?
-                                   <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
-                              </p>
-                            </li>
-                          )
-                        })}
-                      </ul>
-                    </div>
-                </section>
+      
               </div>
             </div>
           </Container>
