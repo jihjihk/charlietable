9de18cf48075b2ba6360f8b751bc55c108ff4bcc
meganhmoore <mm7148@nyu.edu>
2017-12-08 16:51:07 +0400

    Making the dining page connect to the database and making it dynamically assign events to the page

--- web/src/scenes/Dining/index.jsx
+++ web/src/scenes/Dining/index.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './style.css';
+import firebase from 'firebase'
 
-//import firebase, { auth, provider } from './firebase.js';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+import { ref, auth, provider } from '../../services/firebase.js';
 import {
   Button,
   Container,
@@ -15,73 +23,143 @@ import {
   Menu,
   Segment,
   Visibility,
-} from 'semantic-ui-react'
+  Form
+} from 'semantic-ui-react';
+
+import InterestMultiSelect from '../../components/Select/InterestMultiSelect.js';
+import EventTile from './components/EventTile.jsx';
+
+
+var numEvents = 0;
+var cuisines = [];
+var times = [];
+var images = [];
+var locations = [];
 
 export default class Dining extends Component{
   constructor() {
     super();
-    // this.state = {
-    //   user: user
-    // }
+    this.state = {
+      eventName : '',
+      participants: [],
+      time: moment(),
+      city: '',
+      venue: '',
+      cuisine: '',
+      conversationTopic: [],
+      creator: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-    render(){
-      return(
-        <div>
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
-            vertical
-          >
-          <Container text>
-            <Header
-              as='h1'
-              content='Choose Your Dinner'
-              inverted
-              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: 0 }}
-            />
-            <Header
-              as='h2'
-              content='We Will Curate The Rest'
-              inverted
-              style={{ fontSize: '1.7em', fontWeight: 'normal' }}
-            />
-            </Container>
-            <Grid columns={4} divided>
-              <Grid.Row>
-                <Grid.Column>
-                  <Image href='./Images/salad.png' size='medium' style={{ marginTop: '2em' }} />
-                  <Button primary size='medium'>
-                    RSVP
-                    <Icon name='right arrow' />
-                  </Button>
-                </Grid.Column>
-                <Grid.Column>
-                  <Image href='./Images/hamburger.png' size='medium' style={{ marginTop: '2em' }} />
-                  <Button primary size='medium'>
-                    RSVP
-                    <Icon name='right arrow' />
-                  </Button>
-                </Grid.Column>
-                <Grid.Column>
-                  <Image href='./Images/strawberry.png' size='medium' style={{ marginTop: '2em' }} />
-                  <Button primary size='medium'>
-                    RSVP
-                    <Icon name='right arrow' />
-                  </Button>
-                </Grid.Column>
-                <Grid.Column>
-                  <Image href='./Images/salad.png' size='medium' style={{ marginTop: '2em' }} />
-                  <Button primary size='medium'>
-                    RSVP
-                    <Icon name='right arrow' />
-                  </Button>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-        </Segment>
-      </div>
-      );
+
+  handleChange(e) {
+    if(e._isAMomentObject){
+      this.setState({time: e});
+    }else{
+      this.setState({
+        [e.target.name]: e.target.value
+      });
     }
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const eventsRef = firebase.database().ref('events');
+
+    //we can use the imported modules from /services/firebase.js and replace firebase.auth() with auth
+    const user = auth.currentUser;
+    console.log(user);
+
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+
+    createEventTile: function(location, time, cuisine, id, source){
+      return (<EventTile source={source}key={source}/>);
+    };
+
+    const eventsRef = firebase.database().ref('events');
+    const imageRef = firebase.database().ref('diningImages');
+    createEventTiles: function (eventsRef){
+      eventsRef.once("value")
+      .then(function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+          var key = childSnapshot.key;
+          console.log("Child Key "+key);
+          var buttonId = key;
+          childSnapshot.forEach(function(childSnapshot){
+            var childKey = childSnapshot.key;
+            var childVal = childSnapshot.val();
+            if(childKey == "cuisine"){
+              console.log("Found Cuisine");
+              var cuisine = childVal;
+            }
+            if(childKey == "time"){
+              var time = childVal;
+            }
+            if(childKey == "venue"){
+              var location = childVal;
+            }
+            console.log("el in child: "+ childKey);
+            console.log("Child Value: "+childVal);
+          })
+          //get the image that matches the cuisine type
+          imageRef.once("value")
+          .then(function(snapshot){
+            snapshot.forEach(function(childSnapshot){
+              var key = childSnapshot.key;
+              console.log(key);
+              var val = childSnapshot.value();
+              if(key == cuisine){
+                console.log("FOund matching image for cuisine type");
+                source = val;
+              }
+            })
+          })
+          return(this.createEventTile(location, time, cuisine, id, source));
+        })
+      })
+
+    };
+
+  }
+
+  render(){
+    return(
+      <div>
+      <Segment
+      inverted
+      textAlign='center'
+      style={{ minHeight: 700, padding: '1em 0em' }}
+      vertical
+      >
+      <Container text>
+      <Header
+      as='h1'
+      content='Choose Your Dinner'
+      inverted
+      style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: 0 }}
+      />
+      <Header
+      as='h2'
+      content='We Will Curate The Rest'
+      inverted
+      style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+      />
+      </Container>
+      <Grid columns={4} divided>
+        createEventTiles();
+      </Grid>
+      </Segment>
+      </div>
+    );
+  }
 }
